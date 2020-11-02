5734a889153f7db363e706bfc6b856eb523bc4ab
Dima Yukhymuk <dima.yukhimuk@gmail.com>
2020-11-02 10:58:01 +0200

    Deploy to GH

--- gulp/tasks/clean.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function() {
-  $.gulp.task('clean', function(cb) {
-    return $.del($.config.root, cb);
-  });
-};
-
--- gulp/tasks/html.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function() {
-  $.gulp.task('html:build', function () {
-    return $.gulp.src('./src/**/*.html')
-      .pipe($.gulp.dest($.config.root))
-      .pipe($.browserSync.stream());
-  });
-};
